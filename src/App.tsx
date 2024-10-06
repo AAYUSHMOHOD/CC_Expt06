6c7e53dc0edfb14592ca7bbe902f045ee04af9d4
Aayush <97242000+AAYUSHMOHOD@users.noreply.github.com>
2024-10-06 13:05:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-
 function App() {
   return (
     <main>
       <h1>Welcome</h1>
-      <button onClick={createTodo}>+ new</button>
       <div>
         🥳 App successfully hosted by Aayush. 
         <br />
